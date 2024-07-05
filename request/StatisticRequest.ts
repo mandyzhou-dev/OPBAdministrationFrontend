9881c675f321e8d7f69e80bbbd1b2a7e72f09933
Monange <98monange@gmail.com>
2024-07-06 07:08:35 +0800

    modify the zonedtime display of statistics

--- request/StatisticRequest.ts
+++ request/StatisticRequest.ts
@@ -1,14 +1,15 @@
 import { WorkTimeStatistic } from "@/model/WorkTimeStatistic";
 import axios, { AxiosResponse } from "axios";
+import moment,{ Moment } from "moment";
 
 export class StatisticRequest{
-    getByGroupAndDate = async (groupname:string,start: Date, end:Date): Promise<WorkTimeStatistic[]> => {
+    getByGroupAndDate = async (groupname:string,start: Moment, end:Moment): Promise<WorkTimeStatistic[]> => {
         try{
             
             const response:AxiosResponse = await axios.get(process.env.EXPO_PUBLIC_API_URL+'api/presentor/statistic/work-time-statistic/group/{groupname}',{
                 params:{
-                    start: start,
-                    end: end,
+                    start: start.format(),
+                    end: end.format(),
                 }
             });
             return response.data;
--- service/StatisticService.ts
+++ service/StatisticService.ts
@@ -1,8 +1,12 @@
 import { WorkTimeStatistic } from "@/model/WorkTimeStatistic";
 import { StatisticRequest } from "@/request/StatisticRequest";
+import moment from "moment";
 
 export const getStatisticByGroupAndDate = async (groupname:string,start:Date,end:Date):Promise<WorkTimeStatistic[]>=>{
+    const startMoment = moment().year(start.getFullYear()).month(start.getMonth()).date(start.getDate()).hour(start.getHours()).minute(start.getMinutes()).second(start.getSeconds())
+    const endMoment = moment().year(end.getFullYear()).month(end.getMonth()).date(end.getDate()).hour(end.getHours()).minute(end.getMinutes()).second(end.getSeconds())
+    
     const statisticRequest = new StatisticRequest();
-    let workTimeStatisticList:WorkTimeStatistic[] = await statisticRequest.getByGroupAndDate(groupname,start,end);
+    let workTimeStatisticList:WorkTimeStatistic[] = await statisticRequest.getByGroupAndDate(groupname,startMoment,endMoment);
     return workTimeStatisticList;
 }
