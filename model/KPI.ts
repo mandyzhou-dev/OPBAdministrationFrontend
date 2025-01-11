50be862b703e9ab526c6f784b70469c8355cc8ec
YangZhao <yangzhaocamo@gmail.com>
2025-01-10 16:07:26 -0800

    remove some comment

--- model/KPI.ts
+++ model/KPI.ts
@@ -1,8 +1,8 @@
 export class kpi{
     target: number| undefined;
     bonus: number | undefined;
-    startDateTime: Date | undefined; // 起始时间
-    endDateTime: Date | undefined; // 结束时间
+    startDateTime: Date | undefined; 
+    endDateTime: Date | undefined;
     constructor () {
 
     }
