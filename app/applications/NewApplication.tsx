ed682481fd5cb5bb31e4fc8a90710eb0d342518d
Monange <98monange@gmail.com>
2025-10-01 16:40:32 -0700

    Optimized UI feedback

--- app/applications/NewApplication.tsx
+++ app/applications/NewApplication.tsx
@@ -11,17 +11,17 @@ export default function NewApplication() {
     const [leaveTypeValue, setLeaveTypeValue] = React.useState("");
     const [commentIsRequired, setCommentIsRequired] = React.useState(false);
     const [dateIsRequired, setDateIsRequired] = React.useState(false);
-    const [timeIsRequired,setTimeIsRequired] = React.useState(false);
+    const [timeIsRequired, setTimeIsRequired] = React.useState(false);
     const [commentValue, setCommentValue] = React.useState("");
     const [durationValue, setDurationValue] = React.useState("oneday");
     const [dateValue, setDateValue] = React.useState("");
-    const [timeValue,setTimeValue] = React.useState("");
-    const [rangeStartDate,setRangeStartDate] = React.useState("");
-    const [rangeEndDate,setRangeEndDate] = React.useState("")
-    const [rangeStartIsRequired,setRangeStartIsRequired] = React.useState(false);
-    const [rangeEndIsRequired,setRangeEndIsRequired] = React.useState(false);
-    const [showSuccessAlert,setShowSuccessAlert] = React.useState(false);
-    const [showErrorAlert,setShowErrorAlert] = React.useState(false);
+    const [timeValue, setTimeValue] = React.useState("");
+    const [rangeStartDate, setRangeStartDate] = React.useState("");
+    const [rangeEndDate, setRangeEndDate] = React.useState("")
+    const [rangeStartIsRequired, setRangeStartIsRequired] = React.useState(false);
+    const [rangeEndIsRequired, setRangeEndIsRequired] = React.useState(false);
+    const [showSuccessAlert, setShowSuccessAlert] = React.useState(false);
+    const [showErrorAlert, setShowErrorAlert] = React.useState(false);
 
     const submit = () => {
         if (!leaveTypeValue) {
@@ -32,28 +32,28 @@ export default function NewApplication() {
             setCommentIsRequired(true);
             return;
         }
-        if(durationValue=="oneday"){
-            if(!dateValue){
+        if (durationValue == "oneday") {
+            if (!dateValue) {
                 setDateIsRequired(true);
                 return;
             }
-            if(!timeValue){
+            if (!timeValue) {
                 setTimeIsRequired(true);
                 return;
             }
         }
 
-        if(durationValue=="range"){
-            if(!rangeStartDate){
+        if (durationValue == "range") {
+            if (!rangeStartDate) {
                 setRangeStartIsRequired(true);
                 return;
             }
-            if(!rangeEndDate){
+            if (!rangeEndDate) {
                 setRangeEndIsRequired(true);
                 return;
             }
         }
-        
+
         // console.log(leaveTypeValue);
         // console.log(commentValue);
         // console.log(durationValue);
@@ -63,41 +63,45 @@ export default function NewApplication() {
         setCommentIsRequired(false);
         setDateIsRequired(false);
         setTimeIsRequired(false);
-        if(localStorage.getItem("user") === null){
+        if (localStorage.getItem("user") === null) {
             alert("Submit Failure!")
             return;
-        }   
-        let start,end;
+        }
+        let start, end;
         let username = JSON.parse(localStorage.getItem("user") as string).username
-        if(durationValue=="oneday"){
+        if (durationValue == "oneday") {
             let starttime = timeValue.split('-')[0];
             let endtime = timeValue.split('-')[1];
-            start = moment(dateValue+' '+starttime,"YYYY-MM-DD HH:mm").format();
-            end = moment(dateValue+' '+endtime,"YYYY-MM-DD HH:mm").format();
+            start = moment(dateValue + ' ' + starttime, "YYYY-MM-DD HH:mm").format();
+            end = moment(dateValue + ' ' + endtime, "YYYY-MM-DD HH:mm").format();
         }
-        if(durationValue=="range"){
-            console.log(rangeStartDate+' 00:00')
-            start = moment(rangeStartDate+' 00:00',"YYYY-MM-DD HH:mm").format();
-            end = moment(rangeEndDate+' 23:59',"YYYY-MM-DD HH:mm").format();
+        if (durationValue == "range") {
+            console.log(rangeStartDate + ' 00:00')
+            start = moment(rangeStartDate + ' 00:00', "YYYY-MM-DD HH:mm").format();
+            end = moment(rangeEndDate + ' 23:59', "YYYY-MM-DD HH:mm").format();
         }
-        
         let result = {
             applicant: username,
             start: start,
-            end:end,
-            leaveType:leaveTypeValue,
-            reason:commentValue
+            end: end,
+            leaveType: leaveTypeValue,
+            reason: commentValue
         }
         console.log(result);
         const leaveApplicationRequest = new LeaveApplicationRequest;
-        leaveApplicationRequest.putLeaveApplication(result).then(()=>{
+        leaveApplicationRequest.putLeaveApplication(result).then(() => {
             setShowSuccessAlert(true)
-            setTimeout(()=>{setShowSuccessAlert(false)},1000)}
+
+            //Avoid duplicate submitting. Alert is not a good pactice.
+            setTimeout(() => { alert("Request submitted successfully! You can check the status of your request under 'My Applications'.") }, 1000);
+
+            setTimeout(() => { setShowSuccessAlert(false) }, 1000)
+        }
         ).catch(
-            (error)=>{
+            (error) => {
                 setShowErrorAlert(true);
-                setTimeout(()=>{setShowErrorAlert(false)},1000)
-                
+                setTimeout(() => { setShowErrorAlert(false) }, 1000)
+
             }
         );
 
@@ -106,23 +110,23 @@ export default function NewApplication() {
     }
     return (
         <ScrollView>
-            {showSuccessAlert?
-            (<Alert mx="$2.5" action="success" variant="solid" >
-                <AlertIcon as={InfoIcon} mr="$3" />
-                <AlertText>
-                    Successfully submitted!
-                </AlertText>
-            </Alert>):null}
+            {showSuccessAlert ?
+                (<Alert mx="$2.5" action="success" variant="solid" >
+                    <AlertIcon as={InfoIcon} mr="$3" />
+                    <AlertText>
+                        Successfully submitted!
+                    </AlertText>
+                </Alert>) : null}
             {
-                showErrorAlert?
-                (
-                    <Alert mx="$2.5" action="error" variant="solid" >
-                <AlertIcon as={InfoIcon} mr="$3" />
-                <AlertText>
-                    Failed!
-                </AlertText>
-            </Alert>
-                ):null}
+                showErrorAlert ?
+                    (
+                        <Alert mx="$2.5" action="error" variant="solid" >
+                            <AlertIcon as={InfoIcon} mr="$3" />
+                            <AlertText>
+                                Failed!
+                            </AlertText>
+                        </Alert>
+                    ) : null}
             <RequiredFormControl isRequired={leaveTypeIsRequired}
                 isInvalid={leaveTypeIsRequired}
                 title="Leave Type"
@@ -160,12 +164,12 @@ export default function NewApplication() {
                             <FormControlLabelText>
                                 Day(Format:YYYYMMDD)
                             </FormControlLabelText>
-                            
+
                         </FormControlLabel>
                         <Input
                             size="md"
                         >
-                            <InputField placeholder="YYYYMMDD" onChangeText={(value)=>{setDateValue(value)}} />
+                            <InputField placeholder="YYYYMMDD" onChangeText={(value) => { setDateValue(value) }} />
                         </Input>
                     </FormControl>
                     <FormControl isRequired={timeIsRequired} isInvalid={timeIsRequired}>
@@ -173,13 +177,13 @@ export default function NewApplication() {
                             <FormControlLabelText>
                                 Time(Format:HHmm-HHmm)
                             </FormControlLabelText>
-                            
+
                         </FormControlLabel>
                         <Input
                             size="md"
 
                         >
-                            <InputField placeholder="HHmm-HHmm" onChangeText={(value)=>{setTimeValue(value)}}/>
+                            <InputField placeholder="HHmm-HHmm" onChangeText={(value) => { setTimeValue(value) }} />
                         </Input>
                     </FormControl>
 
@@ -195,7 +199,7 @@ export default function NewApplication() {
                         <Input
                             size="md"
                         >
-                            <InputField placeholder="YYYYMMDD" onChangeText={(value)=>{setRangeStartDate(value)}} />
+                            <InputField placeholder="YYYYMMDD" onChangeText={(value) => { setRangeStartDate(value) }} />
                         </Input>
                     </FormControl>
                     <FormControl isRequired={rangeEndIsRequired} isInvalid={rangeEndIsRequired}>
@@ -208,7 +212,7 @@ export default function NewApplication() {
                             size="md"
 
                         >
-                            <InputField placeholder="YYYYMMDD" onChangeText={(value)=>{setRangeEndDate(value)}}/>
+                            <InputField placeholder="YYYYMMDD" onChangeText={(value) => { setRangeEndDate(value) }} />
                         </Input>
                     </FormControl>
                 </Card>
--- app/register.tsx
+++ app/register.tsx
@@ -16,45 +16,48 @@ export default function Register() {
     const [legalName, setLegalName] = useState('')
     const [address, setAddress] = useState('')
     const [phoneNo, setPhoneNo] = useState('')
-    const [birthdate,setBirthdate] = useState('')
-    const [password,setPassword] = useState('')
-    const [sinno,setSinno] = useState('')
+    const [birthdate, setBirthdate] = useState('')
+    const [password, setPassword] = useState('')
+    const [sinno, setSinno] = useState('')
     const [streetAddress, setStreetAddress] = useState('');
     const [city, setCity] = useState('');
     const [province, setProvince] = useState('');
     const [postalCode, setPostalCode] = useState('');
-    const [showSuccessAlert,setShowSuccessAlert] = useState(false);
-    const [showErrorAlert,setShowErrorAlert] = useState(false);
+    const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+    const [showErrorAlert, setShowErrorAlert] = useState(false);
 
 
 
-    const submit=()=>{
+    const submit = () => {
         const address = `${streetAddress}, ${city}, ${province} ${postalCode}`;
         const legalName = `${firstName}${middleName ? ` ${middleName}` : ''} ${lastName}`;
 
         let formattedBirthdate = birthdate.replace(/(\d{4})(\d{2})(\d{2})/, "$1-$2-$3");
 
         let registerInfo = {
-            username:username,
-            name:nickname,
+            username: username,
+            name: nickname,
             legalname: legalName,
-            email:email,
-            phoneNumber:phoneNo,
-            address:address,
-            birthdate:formattedBirthdate,
-            sinno:sinno,
-            password:password
+            email: email,
+            phoneNumber: phoneNo,
+            address: address,
+            birthdate: formattedBirthdate,
+            sinno: sinno,
+            password: password
         }
-        register(registerInfo,verificationCode).then(()=>{
+        register(registerInfo, verificationCode).then(() => {
             setShowSuccessAlert(true)
-            router.navigate("")
+            alert(`Registration successful! 
+            Please log in with your username (${username}) and the password you created.
+            You will be redirected to the login page in 3 seconds.`);
+            setTimeout(() => { router.navigate("") }, 3000)
 
         }).catch(
-            (error)=>{
+            (error) => {
                 console.log(error)
                 setShowErrorAlert(true);
-                setTimeout(()=>{setShowErrorAlert(false)},10000)
-                
+                setTimeout(() => { setShowErrorAlert(false) }, 10000)
+
             }
         );
         //TODO
@@ -64,7 +67,7 @@ export default function Register() {
     //They donnot need to assign their roles .
     return (
         <ScrollView>
-            
+
             <Center margin={3} >
                 <Text size="2xl"  >Create Account</Text>
             </Center>
@@ -113,14 +116,14 @@ export default function Register() {
                     margin={10}
                     width={"$1/6"}
                     action="positive"
-                    onPress={() => { 
+                    onPress={() => {
                         if (!email) {
                             alert("Please enter your email before sending the verification code.");
-                            return; 
+                            return;
                         }
                         sendCode(email);
                         alert(`Verification code has been sent to ' ${email} '`);
-                     }}
+                    }}
                 ><ButtonText>Send code</ButtonText>
                 </Button>
                 <Input>
@@ -140,7 +143,7 @@ export default function Register() {
                     isRequired>
                     <InputField
                         value={password}
-                        onChangeText={(d)=>setPassword(d)}
+                        onChangeText={(d) => setPassword(d)}
                         placeholder="Set Your Password"
                     />
                 </Input>
@@ -206,63 +209,63 @@ export default function Register() {
                     isRequired>
                     <InputField
                         value={sinno}
-                        onChangeText={(d)=>setSinno(d)}
+                        onChangeText={(d) => setSinno(d)}
                         placeholder="Your SIN Number"
                     />
                 </Input>
 
             </Card><Card margin={3}>
-    <Text color="$text500" lineHeight="$xs">
-        Street Address
-    </Text>
-    <Input 
-        isRequired>
-        <InputField
-            value={streetAddress}
-            onChangeText={(d) => setStreetAddress(d)}
-            placeholder="e.g., 1234 Elm St"
-        />
-    </Input>
-</Card>
-<Card margin={3}>
-    <Text color="$text500" lineHeight="$xs">
-        City
-    </Text>
-    <Input 
-        isRequired>
-        <InputField
-            value={city}
-            onChangeText={(d) => setCity(d)}
-            placeholder="e.g., Vancouver"
-        />
-    </Input>
-</Card>
-<Card margin={3}>
-    <Text color="$text500" lineHeight="$xs">
-        Province
-    </Text>
-    <Input 
-        isRequired>
-        <InputField
-            value={province}
-            onChangeText={(d) => setProvince(d)}
-            placeholder="e.g., BC"
-        />
-    </Input>
-</Card>
-<Card margin={3}>
-    <Text color="$text500" lineHeight="$xs">
-        Postal Code
-    </Text>
-    <Input 
-        isRequired>
-        <InputField
-            value={postalCode}
-            onChangeText={(d) => setPostalCode(d)}
-            placeholder="e.g., V6B 1A1"
-        />
-    </Input>
-</Card>
+                <Text color="$text500" lineHeight="$xs">
+                    Street Address
+                </Text>
+                <Input
+                    isRequired>
+                    <InputField
+                        value={streetAddress}
+                        onChangeText={(d) => setStreetAddress(d)}
+                        placeholder="e.g., 1234 Elm St"
+                    />
+                </Input>
+            </Card>
+            <Card margin={3}>
+                <Text color="$text500" lineHeight="$xs">
+                    City
+                </Text>
+                <Input
+                    isRequired>
+                    <InputField
+                        value={city}
+                        onChangeText={(d) => setCity(d)}
+                        placeholder="e.g., Vancouver"
+                    />
+                </Input>
+            </Card>
+            <Card margin={3}>
+                <Text color="$text500" lineHeight="$xs">
+                    Province
+                </Text>
+                <Input
+                    isRequired>
+                    <InputField
+                        value={province}
+                        onChangeText={(d) => setProvince(d)}
+                        placeholder="e.g., BC"
+                    />
+                </Input>
+            </Card>
+            <Card margin={3}>
+                <Text color="$text500" lineHeight="$xs">
+                    Postal Code
+                </Text>
+                <Input
+                    isRequired>
+                    <InputField
+                        value={postalCode}
+                        onChangeText={(d) => setPostalCode(d)}
+                        placeholder="e.g., V6B 1A1"
+                    />
+                </Input>
+            </Card>
 
 
             <Card margin={3}>
@@ -279,28 +282,28 @@ export default function Register() {
                 </Input>
 
             </Card>
-            {showSuccessAlert?
-            (<Alert mx="$2.5" action="success" variant="solid" >
-                <AlertIcon as={InfoIcon} mr="$3" />
-                <AlertText>
-                    Successfully postted!
-                </AlertText>
-            </Alert>):null}
+            {showSuccessAlert ?
+                (<Alert mx="$2.5" action="success" variant="solid" >
+                    <AlertIcon as={InfoIcon} mr="$3" />
+                    <AlertText>
+                        Successfully postted!
+                    </AlertText>
+                </Alert>) : null}
             {
-                showErrorAlert?
-                (
-                    <Alert mx="$2.5" action="error" variant="solid" >
-                <AlertIcon as={InfoIcon} mr="$3" />
-                <AlertText>
-                    User already exists or verify code is wrong!
-                </AlertText>
-            </Alert>
-                ):null}
+                showErrorAlert ?
+                    (
+                        <Alert mx="$2.5" action="error" variant="solid" >
+                            <AlertIcon as={InfoIcon} mr="$3" />
+                            <AlertText>
+                                User already exists or verify code is wrong!
+                            </AlertText>
+                        </Alert>
+                    ) : null}
             <Button
                 margin={10}
                 width={"$1/6"}
                 action="positive"
-                onPress={() => { submit()}}
+                onPress={() => { submit() }}
             >
                 <ButtonText>Done</ButtonText>
             </Button>
